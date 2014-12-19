d1083f9cbcaf713952eed1bd4a29307be8c815f5
zhukang <274546966@qq.com>
2014-12-19 20:19:26 +0800

    "add ValeData class"

---
+++ src/com/mini/tool/ValueData.java
@@ -0,0 +1,15 @@
+package com.mini.tool;
+/**
+ * value字段实体
+ * @author zhukang
+ *
+ */
+public class ValueData {
+	public String typeName;
+	public String keyWord;
+	public ValueData(String typeName,String keyWord){
+		this.typeName = typeName;
+		this.keyWord = keyWord;
+	}
+
+}
